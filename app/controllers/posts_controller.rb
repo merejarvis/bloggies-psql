838c309d04c3da9cf728812675ed4902a6780907
Azmeer Siddiq <reemza77@gmail.com>
2017-08-10 11:34:30 +0800

    logout done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
 # before_action :authenticate_user!, only: [:new, :edit]
 #use except
   def index
-    render json: current_user.name
+    # render json: current_user.posts
+    @all_posts = current_user.posts
+    # render json: current_user.name
   end
 
   def show
